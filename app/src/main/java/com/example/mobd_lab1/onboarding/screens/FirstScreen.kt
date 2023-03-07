5f5af8b754a5ac6bb7a77a1a160cfd9de0c6fda0
katteremenko <katt.eremenko@gmail.com>
2023-03-07 21:55:07 +0700

    add log in

---
+++ app/src/main/java/com/example/mobd_lab1/onboarding/screens/FirstScreen.kt
@@ -0,0 +1,24 @@
+package com.example.mobd_lab1.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mobd_lab1.R
+
+class FirstScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first_screen, container, false)
+
+
+        return view
+    }
+
+
+}

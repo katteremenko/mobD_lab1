5378f4924e30da0cd5d2974be7c9872ea35d34b3
katte <katt.eremenko@gmail.com>
2023-03-08 14:32:31 +0700

    add dictionary, training and video

--- app/src/main/java/com/example/mobd_lab1/MainActivity.kt
+++ app/src/main/java/com/example/mobd_lab1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.mobd_lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mobd_lab1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Dictionary())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.dictionary -> replaceFragment(Dictionary())
+                R.id.training -> replaceFragment(Training())
+                R.id.video -> replaceFragment(Video())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+
+   }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
+
 }
